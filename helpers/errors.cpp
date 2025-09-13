6b068d07290cd0b6e9434cc8ed49e4ff8bcfb465
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-09-13 17:07:56 +0800

    Move error messages to errors.cpp, minor improvements

---
+++ helpers/errors.cpp
@@ -0,0 +1,101 @@
+#include "errors.h"
+
+#include <sstream>
+
+namespace SimpleGL {
+
+std::runtime_error createGLFWWindowFailed(const std::string &title) {
+    std::ostringstream msg;
+
+    msg
+        << "WINDOW: "
+        << "\"" << title << "\": "
+        << "error occurred when creating window\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error GLADLoadFailed(const std::string &title) {
+    std::ostringstream msg;
+
+    msg
+        << "WINDOW: "
+        << "\"" << title << "\": "
+        << "error occurred when loading glad\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error createShaderProgramFailed(const std::string &label) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER MANAGER: "
+        << "Shader program \"" << label << "\": "
+        << "Failed to create shader program\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error createShaderFailed(const std::string &label, GLenum shaderType) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER MANAGER: "
+        << "Shader program \"" << label << "\": "
+        << "Failed to create shader of type "
+        << shaderType
+        << "\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error shaderCompilationFailed(const std::string &label, const char *log) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER MANAGER: "
+        << "Shader program \"" << label << "\": "
+        << "Shader compilation error: "
+        << log
+        << "\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error shaderProgramLinkingFailed(const std::string &label, const char *log) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER MANAGER: "
+        << "Shader program \"" << label << "\": "
+        << "Shader linking error: "
+        << log
+        << "\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error shaderUniformGetterFailed(const std::string &label, const std::string &name) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER PROGRAM: "
+        << "\"" << label << "\": "
+        << "Uniform with name \"" << name << "\" does not exist\n";
+
+    return std::runtime_error(msg.str());
+}
+
+std::runtime_error shaderAttribGetterFailed(const std::string &label, const std::string &name) {
+    std::ostringstream msg;
+
+    msg
+        << "SHADER PROGRAM: "
+        << "\"" << label << "\": "
+        << "Attribute with name \"" << name << "\" does not exist\n";
+
+    return std::runtime_error(msg.str());
+}
+
+}
---
+++ helpers/errors.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+#include <glad/glad.h>
+
+namespace SimpleGL {
+
+std::runtime_error createGLFWWindowFailed(const std::string& title);
+
+std::runtime_error GLADLoadFailed(const std::string& title);
+
+std::runtime_error createShaderProgramFailed(const std::string& label);
+
+std::runtime_error createShaderFailed(const std::string& label, GLenum shaderType);
+
+std::runtime_error shaderCompilationFailed(const std::string& label, const char* log);
+
+std::runtime_error shaderProgramLinkingFailed(const std::string& label, const char* log);
+
+std::runtime_error shaderUniformGetterFailed(const std::string& label, const std::string& name);
+
+std::runtime_error shaderAttribGetterFailed(const std::string& label, const std::string& name);
+
+}

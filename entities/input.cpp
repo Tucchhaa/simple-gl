fec566bc0f6e866209d77de1ac4816b22a5b0431
Fran <124793609+franbrizuelab@users.noreply.github.com>
2025-11-12 16:39:24 +0800

    Key handle (#19)
    Co-authored-by: Eldar Iusupzhanov <ferrarijed@gmail.com>

--- entities/input.cpp
+++ entities/input.cpp
@@ -1,45 +1,63 @@
-#include "input.h"
-
 #include "window.h"
+#include "input.h"
 
 namespace SimpleGL {
 
 std::shared_ptr<Input> Input::create(const std::weak_ptr<Window>& window) {
-    auto instance = std::shared_ptr<Input>(new Input(window));
+    return std::shared_ptr<Input>(new Input(window));
+}
 
-    return instance;
+void Input::keyCallback(int key, int action) {
+    if (action == GLFW_PRESS) {
+        setKeyState(key, true);
+    } else if (action == GLFW_RELEASE) {
+        setKeyState(key, false);
+    }
+}
+
+void Input::mouseButtonCallback(int button, int action) {
+    m_currentMouseStates[button] = action == GLFW_PRESS;
 }
 
 void Input::process() {
+    m_previousKeyStates = m_currentKeyStates;
+    m_previousMouseStates = m_currentMouseStates;
+
     updateCursorPosition();
     updateDeltaTime();
-
-    if(!window()->isFocused()) {
-        reset();
-        return;
-    }
-
-    m_yPositivePressed = isKeyPressed(GLFW_KEY_W);
-    m_yNegativePressed = isKeyPressed(GLFW_KEY_S);
-    m_xPositivePressed = isKeyPressed(GLFW_KEY_D);
-    m_xNegativePressed = isKeyPressed(GLFW_KEY_A);
 
     if (window()->isCursorPositionFixed) {
         window()->setCursorPositionToCenter();
     }
 }
 
+bool Input::isKeyDown(const int key) const {
+    return m_currentKeyStates[key];
+}
+
 bool Input::isKeyPressed(const int key) const {
-    return glfwGetKey(window()->glfwWindow(), key) == GLFW_PRESS;
+    return m_currentKeyStates[key] && !m_previousKeyStates[key];
+}
+
+bool Input::isKeyReleased(const int key) const {
+    return !m_currentKeyStates[key] && m_previousKeyStates[key];
+}
+
+bool Input::isMouseButtonDown(const int button) const {
+    return m_currentMouseStates[button];
 }
 
 bool Input::isMouseButtonPressed(const int button) const {
-    return glfwGetMouseButton(window()->glfwWindow(), button) == GLFW_PRESS;
+    return m_currentMouseStates[button] && !m_previousMouseStates[button];
+}
+
+bool Input::isMouseButtonReleased(const int button) const {
+    return !m_currentMouseStates[button] && m_previousMouseStates[button];
 }
 
 glm::vec2 Input::axisVec2() const {
-    float axisHorizontal = m_xPositivePressed - m_xNegativePressed;
-    float axisVertical = m_yPositivePressed - m_yNegativePressed;
+    float axisHorizontal = static_cast<int>(isKeyDown(GLFW_KEY_D)) - static_cast<int>(isKeyDown(GLFW_KEY_A));
+    float axisVertical = static_cast<int>(isKeyDown(GLFW_KEY_W)) - static_cast<int>(isKeyDown(GLFW_KEY_S));
 
     return { axisHorizontal, axisVertical };
 }
@@ -54,18 +72,12 @@ glm::vec2 Input::mouseDelta() const {
     return { xMouseDelta, yMouseDelta };
 }
 
-void Input::reset() {
-    m_lastFrameTime = Window::time();
-    m_deltaTime = 0;
-
-    m_yPositivePressed = 0;
-    m_yNegativePressed = 0;
-    m_xPositivePressed = 0;
-    m_xNegativePressed = 0;
-}
-
 void Input::updateCursorPosition() {
     glfwGetCursorPos(window()->glfwWindow(), &m_mouseX, &m_mouseY);
+}
+
+void Input::setKeyState(int key, bool pressed) {
+    m_currentKeyStates[key] = pressed;
 }
 
 void Input::updateDeltaTime() {
--- entities/input.h
+++ entities/input.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <memory>
+#include <array>
 
 #include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 namespace SimpleGL {
 
@@ -12,31 +15,43 @@ class Input {
 public:
     static std::shared_ptr<Input> create(const std::weak_ptr<Window>& window);
 
+    void keyCallback(int key, int action);
+
+    void mouseButtonCallback(int button, int action);
+
     void process();
 
     glm::vec2 axisVec2() const;
 
     glm::vec2 mouseDelta() const;
 
+    bool isKeyDown(int key) const;
     bool isKeyPressed(int key) const;
+    bool isKeyReleased(int key) const;
 
+    bool isMouseButtonDown(int button) const;
     bool isMouseButtonPressed(int button) const;
+    bool isMouseButtonReleased(int button) const;
 
     float deltaTime() const { return m_deltaTime; }
 
+    void setKeyState(int key, bool pressed);
+
 private:
+
     std::weak_ptr<Window> m_window;
 
     float m_lastFrameTime = 0;
     float m_deltaTime = 0;
 
-    float m_yPositivePressed = 0;
-    float m_yNegativePressed = 0;
-    float m_xPositivePressed = 0;
-    float m_xNegativePressed = 0;
-
     double m_mouseX = 0;
     double m_mouseY = 0;
+
+    std::array<bool, GLFW_KEY_LAST + 1> m_currentKeyStates{};
+    std::array<bool, GLFW_KEY_LAST + 1> m_previousKeyStates{};
+
+    std::array<bool, GLFW_MOUSE_BUTTON_LAST + 1> m_currentMouseStates{};
+    std::array<bool, GLFW_MOUSE_BUTTON_LAST + 1> m_previousMouseStates{};
 
     explicit Input(const std::weak_ptr<Window> &window): m_window(window) {};
 
